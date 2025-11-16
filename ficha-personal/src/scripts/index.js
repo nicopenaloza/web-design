6fdeae54496244844778986811a29446c77fd26b
Nicolas Peñaloza <nicopenaloza@gmail.com>
2025-11-15 23:25:12 -0300

    Update ficha personal

--- ficha-personal/src/scripts/index.js
+++ ficha-personal/src/scripts/index.js
@@ -1,13 +1,8 @@
-const toggleMenu = () => {
-    const menu = document.getElementById("menu");
-    const menuButton = document.getElementById("menu-button");
-    menu.classList.toggle("menu-active")
-    menuButton.classList.toggle("menu-button-active");
-}
 
-const toggleTheme = () => {
+const toggleTheme = (ev) => {
     const body = document.querySelector("body");
     body.classList.toggle("light-mode");
+    ev.target.innerText = body.classList.contains("light-mode") ? "🌙" : "☀️";
 }
 
 const toggleThemeButtons = document.querySelectorAll('.theme-toggle-btn');
